f2de8bed83a29fb0880854ad388049910ab22b7a
Tom Salisbury <Tom.Salisbury@oneiota.co.uk>
2024-12-10 12:59:40 +0000

    file tidy up

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Wonders of Ancient Egypt';
+export const SITE_DESCRIPTION = 'This is the site for all things Ancient Egypt, mythology and for exploring the wonders of ancient earth.';
